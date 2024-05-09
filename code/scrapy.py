d1cf72659aa1e3e5112f201052b31e9c2622d7d8
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2024-05-09 17:04:40 +0900

    save

--- code/scrapy.py
+++ code/scrapy.py
@@ -16,6 +16,9 @@ def md(html, **options):
 
 url = "https://www.psychologytoday.com/intl/blog/all-is-well/202405/who-decides-what-art-is-good"
 url = "https://www.psychologytoday.com/intl/blog/denying-to-the-grave/202405/consequences-of-being-mistreated-by-the-healthcare-system"
+url = "https://www.psychologytoday.com/intl/blog/speaking-in-tongues/202405/financial-infidelity-the-cost-of-keeping-secrets"
+
+base_url = "https://www.psychologytoday.com/intl"
 
 
 
@@ -23,7 +26,7 @@ url = "https://www.psychologytoday.com/intl/blog/denying-to-the-grave/202405/con
 with urllib.request.urlopen(url) as u:
     markdown = md(u.read())
     for row in markdown.split("\n"):
-        if len(row) < 300:
+        if len(row) < 100:
             continue
         translated = translator.translate(row, dest="ja");
         print(translated.text) # Japanese
