de9192b805b9d637b791ee4103d6e666daea3197
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2024-05-12 19:41:53 +0900

    札幌の家系ラーメンは世界一

--- code/workflows.py
+++ code/workflows.py
@@ -7,7 +7,30 @@ HATENA_ID = "minegishirei"
 BLOG_DOMAIN = "psy.hatenadiary.com"
 API_KEY = "u6v0f3440e"
 
-def hatena_entry(title, contents, entry_id, categorys=[], updated="", draft=False):
+def hatena_create_entry(title, contents, categorys=[], updated="", draft=False):
+    BASE_URL = f"https://blog.hatena.ne.jp/minegishirei/{BLOG_DOMAIN}/atom/entry"
+    updated = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    category = lambda x: "\n".join([f"<category term='{e}' />" for e in x])
+    categorys = category(categorys) if category else ""
+
+    xml = f"""<?xml version="1.0" encoding="utf-8"?>
+    <entry xmlns="http://www.w3.org/2005/Atom"
+          xmlns:app="http://www.w3.org/2007/app">
+      <title>{title}</title>
+      <author><name>name</name></author>
+      {categorys}
+      <updated>{updated}</updated>
+      <content type="text/x-markdown">
+        {contents}
+      </content>
+    </entry>""".encode(
+        "UTF-8"
+    )
+    r = req.post(BASE_URL, auth=(HATENA_ID, API_KEY), data=xml)
+    return r.text
+
+
+def hatena_update_entry(title, contents, entry_id, categorys=[], updated="", draft=False):
     BASE_URL = f"https://blog.hatena.ne.jp/minegishirei/{BLOG_DOMAIN}/atom/entry/{entry_id}"
     updated = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     category = lambda x: "\n".join([f"<category term='{e}' />" for e in x])
@@ -66,7 +89,14 @@ if __name__ == "__main__":
         title, categorys, entry_id, *content = f.readlines()
     categorys = categorys.split(",")
     content = "\n".join(content)
-    r = hatena_entry(title , escape_xml(content), entry_id, categorys,True, False)
-    print(r)
-    if "400 XML Parse Failed" in r:
-        print(escape_xml(content))
+    if len(entry_id) > 0:
+        r = hatena_update_entry(title , escape_xml(content), entry_id, categorys,True, False)
+        print(r)
+        if "400 XML Parse Failed" in r:
+            print(escape_xml(content))
+    else:
+        r = hatena_create_entry(title , escape_xml(content), categorys,True, False)
+        print(r)
+        if "400 XML Parse Failed" in r:
+            print(escape_xml(content))
+
