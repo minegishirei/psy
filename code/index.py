fa448c3b81bb01c30f1e599f6305470ad1aed818
峰岸零 <minegishirei@minegishireinoMacBook-Air.local>
2025-02-25 15:07:01 +0900

    test

--- code/index.py
+++ code/index.py
@@ -6,6 +6,7 @@ import itertools
 import json
 import itertools
 from urllib.parse import urlparse
+import urllib
 from chatgpt import get_client, call_groq
 from scrapy import get_done_url_list, get_links, create_link, add_done
 from eng_html_to_jp_md.main import create_japanese_sentence
@@ -170,7 +171,7 @@ hhhhhhhhhhhhhhhhhhh
 
 def get_prompt_template(sentence):
     return f"""
-次の記事から得られる実用的な心理学の定理を日本語で導き出して！(複数の効果を導き出しても良いです)
+後述する記事から得られる実用的な心理学の定理を、次の形式で、**日本語で** 出力してください(複数可)
 
 ## 効果:<タイトル>
 - action:<タイトル>
@@ -178,23 +179,32 @@ def get_prompt_template(sentence):
 - effect:<予想できる事象>
 - example:<具体例>
 
-<記事の要約>
+以下は具体例です。
 
-以下要約するターゲットです。（日本語でお願いします。）
+## 効果:一貫性の法則
+- action:一貫性の法則
+- content:一貫性を保つことは社会生活において他者から高い評価を受ける
+- example:好きな歌手のCDだからという理由だけで買ってしまう
+- effect:同じ行動を取り続ける
+
+以下要約するターゲットです。
 
 {sentence}
 
     """
 
 
+
 if __name__ == "__main__":
     sites = [
+        #"https://ja.wikipedia.org/wiki/%E3%82%AB%E3%83%AA%E3%82%AE%E3%83%A5%E3%83%A9%E5%8A%B9%E6%9E%9C",#カリギュラ効果:良い
         # "https://www.psychologistworld.com/dreams/", #夢
         # "https://www.psychologistworld.com/",
         # "https://www.simplypsychology.org/regression-defence-mechanism.html", #退行:forbidden
-        "https://newstyle.link/category58/", #口癖
-        "https://japan-brain-science.com/archives/3618",
+        # "https://newstyle.link/category58/", #口癖:根拠がない
+        # "https://japan-brain-science.com/archives/3618", #低品質
         #"https://japan-brain-science.com/",
+        "https://psychologist.x0.com/terms/144.html#btm", #エリクソンのライフサイクル理論
         "https://psychcentral.com/",
         "https://ja.wikipedia.org/wiki/%E5%A4%A7%E8%84%B3", #低変質
         #"https://www.apa.org/",
@@ -218,7 +228,13 @@ if __name__ == "__main__":
             if url in done_url_list:
                 continue
             count += 1
-            sentence = run_scrapy(url)
+            sentence = ""
+            try:
+                sentence = run_scrapy(url)
+            except AttributeError:
+                pass
+            except urllib.error.HTTPError:
+                pass
             result = ""
             try:
                 result = run_chatgpt(get_prompt_template(sentence)[:6000] )
